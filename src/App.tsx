234b3faaaf2fd6eafae637daaa56350d2928defd
Dodô <evandro.carreira@gmail.com>
2023-01-29 13:45:26 -0300

    refactor(vite): change to vite with react and typescript

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Controladora de brisas</h1>
+      <div className="card">
+        video
+      </div>
+      <div className='card'>
+        shader
+      </div>
+      <div className='card'>
+        <ol>
+          <li>Drag & Drop</li>
+          <li>Video</li>
+          <li>video final</li>
+          <li>conector para input</li>
+          <li>popup de video em nova janela</li>
+          <li>filtro shader</li>
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default App
